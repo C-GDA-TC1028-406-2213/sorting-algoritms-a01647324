a048147572daa113360bdd58545a80e78bef111a
branep23 <brandonep.star2310@gmail.com>
2025-08-20 22:46:13 -0600

    Update bubble and selected

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -8,11 +8,30 @@
 namespace SortingAlgorithms {
 
 void bubbleSort(int arr[], int n) {
-    
+    int var = 0;
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                var = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = var;
+            }
+        }
+    }
 }
 
 void selectionSort(int arr[], int n) {
-    
+    for(int i=0; i<n-1; i++){
+        int minindex = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[minindex]){
+                minindex = j;
+            }
+        }
+        int var = arr[i];
+        arr[i] = arr[minindex];
+        arr[minindex] = var;
+    }
 }
 
 void insertionSort(int arr[], int n) {
